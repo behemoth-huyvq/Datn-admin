26b76bdf009dd850cbf6f1efc49de2f99e0a6cec
behemoth-huyvq <huyvq@behemoth.vn>
2020-04-19 23:46:25 +0700

    base-layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,8 @@ class User < ApplicationRecord
   # self.table_name = "USER"
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :recoverable, :registerable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :rememberable, :validatable
 
   has_many :user_roles
   has_many :roles, through: :user_roles
